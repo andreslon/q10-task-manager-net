4067129369c3da37c5b712a90b6422e76b5b643e
andreslon <andreslon@outlook.com>
2025-11-11 08:34:50 -0500

    Update configuration for database connections and Kubernetes deployment
    - Changed database connection string ports from 15432 to 5432 in launchSettings.json and config-map.yml.
    - Updated RabbitMQ host in RabbitMQRepository.cs from localhost to a specific IP address.
    - Added namespace to Kubernetes config-map, deployment, and service files.
    - Updated container port in deployment and service files from 80 to 8080, aligning with the new application configuration.

--- api/Q10.TaskManager.Infrastructure/Repositories/RabbitMQRepository.cs
+++ api/Q10.TaskManager.Infrastructure/Repositories/RabbitMQRepository.cs
@@ -15,7 +15,7 @@ namespace Q10.TaskManager.Infrastructure.Repositories
         {
             var factory = new ConnectionFactory
             {
-                HostName = "localhost",
+                HostName = "10.104.182.199",
                 Port = 5672,
                 UserName = "admin",
                 Password = "admin123"
